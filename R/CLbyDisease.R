f71ac58ecd8237639ca24234d77cc341013459ce
Daniel Osorio <dcosorioh@tamu.edu>
2021-10-06 14:50:51 -0500

    Create CLbyDisease.R

---
+++ R/CLbyDisease.R
@@ -0,0 +1,6 @@
+#' @docType data
+#' @name CLbyDisease
+#' @title CLbyDisease
+#' @usage data(CLbyDisease)
+#' @format A list of 14 cancer types and the cell lines used as surrogates by the LINCS-L1000 project.
+NULL
